bee6493807aef6e6eda3bd881f91bd38fd7d81c9
Michael DeCicco <mdecicco8@gmail.com>
2024-08-26 23:55:02 -0400

    forgot to remove this

--- include/utils/types.h
+++ include/utils/types.h
@@ -15,22 +15,4 @@ namespace utils {
     typedef int8_t      i8;
     typedef float       f32;
     typedef double      f64;
-    typedef i32         type_id;
-
-    struct type_meta {
-        unsigned size : 16;
-        unsigned is_pod : 1;
-        unsigned is_trivially_constructible : 1;
-        unsigned is_trivially_copyable : 1;
-        unsigned is_trivially_destructible : 1;
-        unsigned is_primitive : 1;
-        unsigned is_floating_point : 1;
-        unsigned is_integral : 1;
-        unsigned is_unsigned : 1;
-        unsigned is_function : 1;
-        unsigned is_pointer : 1;
-        unsigned is_alias : 1;
-        unsigned is_enum : 1;
-        unsigned __pad0 : 4;
-    };
 };
